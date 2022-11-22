8aede28de7204e1f9c352233bdfa1ac0f0338fab
kangxuan <895560759@qq.com>
2022-11-22 15:44:06 +0800

    init

---
+++ the-way-to-go/chapter_4/1_hello_world.go
@@ -0,0 +1,13 @@
+package main
+
+// 导入fmt包
+import "fmt"
+
+// main函数，一个执行程序有且只有一个main函数
+func main() {
+	// fmt包的Println函数输出并换行
+	fmt.Println("Hello World")
+}
+
+// go run 1_hello_world.go
+// Hello World
---
+++ the-way-to-go/chapter_4/2_alias.go
@@ -0,0 +1,11 @@
+package main
+
+// 给包取别名
+import fm "fmt"
+
+func main() {
+	fm.Println("hello, world")
+}
+
+// go run 2_alias.go
+// Hello World
