783c5edbb71784203a6143ab3276df093a2403ec
kangxuan <895560759@qq.com>
2022-12-16 16:59:59 +0800

    增加HTML转义的代码

--- go-web/chapter_3/3.go
+++ go-web/chapter_3/3.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 // login 实现登录的逻辑
@@ -14,13 +15,23 @@ func login(w http.ResponseWriter, r *http.Request) {
 		t, _ := template.ParseFiles("3_login.html")
 		log.Println(t.Execute(w, nil))
 	} else {
-		err := r.ParseForm()
+		err := r.ParseForm() // 解析form表单数据
 		if err != nil {
 			log.Fatal("ParseForm: ", err)
 		}
 
+		v := url.Values{} // request.Form 是一个 url.Values 类型
+		v.Set("name", "Ava")
+		fmt.Println("name: ", v.Get("name"))
+
 		fmt.Println("username: ", r.Form["username"])
 		fmt.Println("password: ", r.Form["password"])
+
+		fmt.Println("转义之后")
+		fmt.Println("username: ", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password: ", template.HTMLEscapeString(r.Form.Get("password")))
+
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) // 输出到客户端
 	}
 }
 
@@ -36,5 +47,9 @@ func main() {
 //method:  GET
 //2022/12/16 16:26:53 <nil>
 //method:  POST
+//name:  Ava
 //username:  [shanla]
 //password:  [123456]
+//转义之后
+//username:  shanla
+//password:  123456
