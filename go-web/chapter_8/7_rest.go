eec78d13ae71c6d7fc3168063e41d57d6e4785bc
kangxuan <895560759@qq.com>
2023-02-07 19:53:40 +0800

    rest

---
+++ go-web/chapter_8/7_rest.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	router.GET("/getuser/:uid", getuser)
+	router.POST("/adduser/:uid", adduser)
+	router.DELETE("/deluser/:uid", deleteuser)
+	router.PUT("/moduser/:uid", modifyuser)
+
+	log.Fatal(http.ListenAndServe(":8089", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func getuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are get user %s", uid)
+}
+
+func adduser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are add user %s", uid)
+}
+
+func deleteuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are delete user %s", uid)
+}
+
+func modifyuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are modify user %s", uid)
+}
