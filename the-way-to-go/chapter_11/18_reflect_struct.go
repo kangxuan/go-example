2cf8b86fbce0b0de3ed87c9caef7906d7507cb1e
kangxuan <895560759@qq.com>
2023-03-15 16:22:22 +0800

    reflect

--- the-way-to-go/chapter_11/18_reflect_struct.go
+++ the-way-to-go/chapter_11/18_reflect_struct.go
@@ -9,6 +9,7 @@ type NotknownType struct {
 	s1, s2, s3 string
 }
 
+// String 如果类型有String()方法，通过fmt.Print打印时会调用这个方法
 func (n NotknownType) String() string {
 	return n.s1 + "-" + n.s2 + "-" + n.s3
 }
@@ -24,10 +25,13 @@ func main() {
 	knd := value.Kind() // struct
 	fmt.Println(knd)
 
+	// v.NumField()返回字段数量
+	// v.Field(i)返回具体某一个字段的值
 	for i := 0; i < value.NumField(); i++ {
 		fmt.Printf("Field %d: %v\n", i, value.Field(i))
 	}
 
+	// Method(i)返回具体某一个方法，Call()调用此方法
 	results := value.Method(0).Call(nil)
 	fmt.Println(results) // [Ada - Go - Oberon]
 }
