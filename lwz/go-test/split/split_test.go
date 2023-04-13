b49b640fae408dfed67755fc459b170848e55d67
kangxuan <895560759@qq.com>
2023-04-13 15:15:20 +0800

    go test 例子

--- lwz/go-test/split/split_test.go
+++ lwz/go-test/split/split_test.go
@@ -13,16 +13,25 @@ func TestSplit(t *testing.T) {
 	}
 }
 
-func TestMoreSplit(t *testing.T) {
-	got := Split("abcd", "bc")
-	want := []string{"a", "d"}
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("expected:%v, got:%v", want, got)
-	}
-}
+//func TestMoreSplit(t *testing.T) {
+//	got := Split("abcd", "bc")
+//	want := []string{"a", "d"}
+//	if !reflect.DeepEqual(want, got) {
+//		t.Errorf("expected:%v, got:%v", want, got)
+//	}
+//}
 
 func BenchmarkSplit(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Split("a:B:c", ":")
 	}
+}
+
+func BenchmarkSplitParallel(b *testing.B) {
+	// b.SetParallelism(1) // 设置使用的CPU数
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			Split("沙河有沙又有河", "沙")
+		}
+	})
 }
