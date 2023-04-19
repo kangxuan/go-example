3e5dc635d27b5ada3898c41959d9a17eaffc9f05
kangxuan <895560759@qq.com>
2023-04-19 17:29:31 +0800

    定时器

--- the-way-to-go/chapter_14/19_timer_goroutine.go
+++ the-way-to-go/chapter_14/19_timer_goroutine.go
@@ -6,19 +6,21 @@ import (
 )
 
 func main() {
+	// 每隔1秒流出一个"当前时间"
 	tick := time.Tick(time.Second)
+	// 5秒后返回流出一个"当前时间"
 	boom := time.After(5 * time.Second)
 
 	for true {
 		select {
-		case <-tick:
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("boom.")
+		case ct := <-tick:
+			fmt.Println(ct)
+		case ct := <-boom:
+			fmt.Println(ct)
 			return
 		default:
 			fmt.Println("    .")
-			time.Sleep(1e9)
+			time.Sleep(time.Second)
 		}
 	}
 }
